e6515fc0fb55ec61d79bb52dd5a3d8a33ebe4790
Miraz <mirazalmamun@gmail.com>
2018-01-26 14:28:18 +1100

    files(add): first set of files added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Index file
+ *
+ * @since
+ * @author Miraz Al-Mamun<mirazmamun@yahoo.com>
+ */
+module.exports = () => {
+  console.log('This module is under development. Please hang tight. Final product underway. 👏');
+}
